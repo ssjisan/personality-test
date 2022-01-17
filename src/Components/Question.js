59d61e11efcb05a9848d39854d27b156f5afc1f6
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2022-01-17 10:54:00 +0600

    question set done

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -78,6 +78,7 @@ export default function Question() {
     countResult();
     const nextQuestion = currentQuestionNo + 1;
     setCurrentQuestionNo(nextQuestion);
+    setButton()
   };
   const handleFinish = () => {
     countResult();
